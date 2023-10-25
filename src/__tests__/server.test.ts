68845a1d9574634b365daf8e78ccba352ed5224e
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-25 16:53:48 -0600

    first commit

---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest'
+import server, { connectDB } from '../server'
+import db from '../config/db'
+
+
+describe('GET /api', () => {
+    it('should send back a json response', async () => {
+        const res = await request(server).get('/api')
+
+        expect(res.status).toBe(200)
+        expect(res.headers['content-type']).toMatch(/json/)
+        expect(res.body.msg).toBe('Desde API')
+
+        expect(res.status).not.toBe(404)
+        expect(res.body.msg).not.toBe('desde api')
+    })
+})
+
+jest.mock('../config/db')
+
+describe('connectDB', () => {
+    it('should handle database connection error', async () => {
+        jest.spyOn(db, 'authenticate')
+            .mockRejectedValueOnce(new Error('Hubo un error al conectar a la BD'))
+        const consoleSpy = jest.spyOn(console, 'log')
+
+        await connectDB()
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            expect.stringContaining('Hubo un error al conectar a la BD')
+        )
+    })
+})
