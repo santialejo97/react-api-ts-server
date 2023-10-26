bfe0ebe2ff6bca3d1a53582a1b11b505cf9d6ded
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-25 18:03:31 -0600

    03

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,31 @@ import { handleInputErrors } from './middleware'
 
 const router = Router()
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: The Product ID
+ *                      example: 1
+ *                  name:
+ *                      type: string
+ *                      description: The Product name
+ *                      example: Monitor Curvo de 49 Pulgadas
+ *                  price:
+ *                      type: number
+ *                      description: The Product price
+ *                      example: 300
+ *                  availability:
+ *                      type: boolean
+ *                      description: The Product availability
+ *                      example: true
+ */
+
 // Routing
 router.get('/', getProducts)
 router.get('/:id', 
