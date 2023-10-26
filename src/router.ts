9cb1ee4b5d9bffc2eeb78d61dc0efc5844259901
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-25 19:12:58 -0600

    07

--- src/router.ts
+++ src/router.ts
@@ -107,7 +107,11 @@ router.get('/:id',
  *                              example: 399
  *      responses:
  *          201:
- *              description: Product created successfully
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
  *          400:
  *              description: Bad Request - invalid input data
  * 
@@ -124,6 +128,49 @@ router.post('/',
     createProduct
 )
 
+/**
+ * @swagger
+ * /api/products/{id}:  
+ *  put:
+ *      summary: Updates a product with user input
+ *      tags:
+ *          - Products
+ *      description: Returns the updated product
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to retrieve
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: "Monitor Curvo 49 Pulgadas"
+ *                          price:
+ *                              type: number
+ *                              example: 399
+ *                          availability:
+ *                              type: boolean
+ *                              example: true
+ *      responses:
+ *          200:
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad Request - Invalid ID or Invalid input data
+ *          404:
+ *              description: Product Not Found
+ */
 
 router.put('/:id', 
     param('id').isInt().withMessage('ID no válido'),
@@ -138,6 +185,7 @@ router.put('/:id',
     handleInputErrors,
     updateProduct
 )
+
 
 router.patch('/:id', 
     param('id').isInt().withMessage('ID no válido'),
