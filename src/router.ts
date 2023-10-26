84a860ded66f1dc16f21f19d74b59ac2e72856ab
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-26 11:05:02 -0600

    08

--- src/router.ts
+++ src/router.ts
@@ -186,6 +186,33 @@ router.put('/:id',
     updateProduct
 )
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  patch:
+ *      summary: Update Product availability
+ *      tags: 
+ *          - Products
+ *      description: Returns the updated availability
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to retrieve
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad Request - Invalid ID
+ *          404:
+ *              description: Product Not Found
+ */
 
 router.patch('/:id', 
     param('id').isInt().withMessage('ID no válido'),
