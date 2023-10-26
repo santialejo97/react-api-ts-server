2c64aa360907b93c25a2cd63b206e59481c2c4bc
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-26 11:15:25 -0600

    09

--- src/router.ts
+++ src/router.ts
@@ -220,6 +220,36 @@ router.patch('/:id',
     updateAvailability
 )
 
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  delete:
+ *      summary: Deletes a product by a given ID
+ *      tags: 
+ *          - Products
+ *      description: Returns a confirmation message
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to delete
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: string
+ *                          value: 'Producto Eliminado'
+ *          400:
+ *              description: Bad Request - Invalid ID
+ *          404:
+ *              description: Product Not Found
+ */
+
 router.delete('/:id', 
     param('id').isInt().withMessage('ID no válido'),
     handleInputErrors,
