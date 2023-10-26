a855f033e6a6a7693076b8476d0eb058fba1286c
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-25 18:41:49 -0600

    05

--- src/router.ts
+++ src/router.ts
@@ -51,7 +51,35 @@ const router = Router()
  */
 router.get('/', getProducts)
 
-
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  get:
+ *      summary: Get a product by ID
+ *      tags:
+ *          - Products
+ *      description: Return a product based on its unique ID
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to retrieve
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Successful Response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          404:
+ *              description: Not found
+ *          400:
+ *              description: Bad Request - Invalid ID
+ * 
+ * 
+ */
 router.get('/:id', 
     param('id').isInt().withMessage('ID no válido'),
     handleInputErrors,
