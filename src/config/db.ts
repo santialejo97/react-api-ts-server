0422fbb78ff1ce8df497d3eaab2ca6c570136067
Santiago Gaviria <gaviriasantiago1997@gmail.com>
2024-10-14 14:19:08 -0500

    first commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,10 @@
-import { Sequelize } from 'sequelize-typescript'
-import dotenv from 'dotenv'
-dotenv.config()
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*.ts'],
-    logging: false
-})
+  models: [__dirname + "/../models/**/*"],
+  logging: false,
+});
 
-export default db
+export default db;
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,53 @@
-import express from 'express' 
-import colors from 'colors'
-import swaggerUi from 'swagger-ui-express'
-import swaggerSpec, { swaggerUiOptions } from './config/swagger'
-import router  from './router'
-import db from './config/db'
+import express from "express";
+import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
+import router from "./router";
+import db from "./config/db";
 
 // Conectar a base de datos
 export async function connectDB() {
-    try {
-        await db.authenticate()
-        db.sync()
-        // console.log( colors.blue( 'Conexión exitosa a la BD'))
-    } catch (error) {
-        // console.log(error)
-        console.log( colors.red.bold( 'Hubo un error al conectar a la BD') )
-    }
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue("Conexión exitosa a la BD"));
+  } catch (error) {
+    // console.log(error)
+    console.log(colors.red.bold("Hubo un error al conectar a la BD"));
+  }
 }
-connectDB()
+connectDB();
 
 // Instancia de express
-const server = express()
+const server = express();
 
 // Leer datos de formularios
-server.use(express.json())
+server.use(express.json());
 
-server.use('/api/products', router)
+const corsOption: CorsOptions = {
+  origin: function (origine, callback) {
+    console.log(origine);
+    if (origine == process.env.URL_FRONTEND) {
+      console.log("permitir");
+      callback(null, true);
+    } else {
+      console.log("denegar");
+      callback(new Error("Error de cors"), false);
+    }
+  },
+};
+
+server.use(cors(corsOption));
+server.use(morgan("dev"));
+server.use("/api/products", router);
 
 // Docs
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions) )
+server.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, swaggerUiOptions)
+);
 
-export default server
+export default server;
